2b62932bbd220adebbf96c69709a603f1bd5df39
Fanny Lundberg <93767753+FannyLundberg@users.noreply.github.com>
2022-05-23 11:38:42 +0200

    Startat projekt, satt upp koppling mot databas

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+const cors = require("cors");
+
+const MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect("mongodb://localhost:27017", {
+    useUnifiedTopology: true
+})
+.then(client => {
+    console.log("Uppkopplad mot databasen")
+
+    const db = client.db("newsletter");
+
+    app.locals.db = db;
+})
+
+app.use(cors());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
