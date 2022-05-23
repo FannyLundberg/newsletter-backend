2d9cb4c6b06a61311d123312c0dd8ed4aeeb9188
Fanny Lundberg <93767753+FannyLundberg@users.noreply.github.com>
2022-05-23 15:10:22 +0200

    Kan kontrollera användare som försöker logga in och skapa ny användare

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+// Kontrollera om användarnamn och lösenord stämmer överens 
+router.post("/", (req, res) => {
+
+  req.app.locals.db.collection("users").find().toArray()
+  .then(results => {
+    console.log(results);
+
+    let existingUser = results.find((user) => {
+      return user.username == req.body.username && user.password == req.body.password
+    })
+
+    // Om användaren har angett rätt och matchande uppgifter
+    // Skicka med användarens id
+    if(existingUser) {
+      return res.json({ "message": "success", "userId": existingUser._id })
+
+    // Om fel uppgifter: error
+    } else {
+      res.json({ "message": "error" })
+    }
+  });
+});
+
+
+// Lägga till en ny användare
+router.post("/newuser", (req, res) => {
+
+  req.app.locals.db.collection("users").insertOne(req.body)
+  .then(result => {
+    console.log(result);
+
+    res.json({ "message": "success" })
+  })
+})
+
+
+// Hämta alla användare
+router.get("/", (req, res) => {
+
+  req.app.locals.db.collection("users").find().toArray()
+  .then(results => {
+    console.log(results)
+    
+    res.json({ "message": "success", "resultat": results })
+  })
 });
 
 module.exports = router;
